035131770a641eb51631ac3e41feb6e4a0b9b9f6
RyuseiAkaike <89002202+RyuseiAkaike@users.noreply.github.com>
2021-09-17 17:23:01 +0900

    mode0におけるdeepsleepmodeの追加

--- program/M5_OLIPO/M5_OLIPO.ino
+++ program/M5_OLIPO/M5_OLIPO.ino
@@ -1,5 +1,3 @@
-//1.1.0ロータリースイッチによるmodeの変更要素を追加
-//1.2.0modeを二種類に変更
 #include <M5StickCPlus.h>
 
 #define SWITCH 33
@@ -7,8 +5,11 @@
 int switch_value;
 
 void setup() {
+  Serial.begin(9600);
   M5.begin();
-  pinMode(switch_value, INPUT);
+  pinMode(SWITCH,INPUT);
+//  pinMode(GPIO_NUM_37, INPUT_PULLUP);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, HIGH);
 }
 
 void loop() {
@@ -18,19 +19,26 @@ void loop() {
 
   switch_value = analogRead(SWITCH);
 
-  if (switch_value < 400) {
+  if (switch_value < 200) {
+    //    M5.Lcd.setTextColor(RED);
+    //    M5.Lcd.setTextSize(3);
+    //    M5.Lcd.setCursor(10, 2);
+    //    M5.Lcd.println("mode0");
+
+    M5.Axp.ScreenBreath(0);
+    esp_deep_sleep_start();
+  }
+  if (switch_value >= 200 && switch_value < 400) {
     M5.Lcd.setTextColor(GREEN);
     M5.Lcd.setTextSize(3);
     M5.Lcd.setCursor(10, 2);
-    M5.Lcd.println("mode0");
+    M5.Lcd.println("mode1");
   }
-
-    if (switch_value >= 400 && switch_value < 1000) {
+  if (switch_value >= 400 && switch_value < 1000) {
     M5.Lcd.setTextColor(ORANGE);
     M5.Lcd.setTextSize(3);
     M5.Lcd.setCursor(10, 2);
     M5.Lcd.println("mode2");
   }
-
 
 }
